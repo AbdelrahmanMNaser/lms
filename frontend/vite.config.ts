f658e77e3b9fb56b49e8b5c7a97c873363072bc1
AbdelrahmanMNaser <sim.abdelrahmanmohamed36283@alexu.edu.eg>
2026-05-02 04:56:35 +0300

    feat: integrate Tailwind CSS and update Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+export default defineConfig(({ mode }) => {
+  const env : any = loadEnv(mode, process.cwd(), '');
+  const PORT : number = parseInt(env.VITE_CLIENT_PORT) || 5173;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: PORT,
+    },
+    preview: {
+      port: PORT,
+    },
+  }
 })
